8de8660e4018001b0c8df10d7d909ad3dd2c2d6d
MohammedAlhawamdeh <hawamdeh9393@gmail.com>
2019-11-16 11:37:34 +0200

    sumArray

--- app.js
+++ app.js
@@ -92,11 +92,24 @@ var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
 
+    var questionFourReturnArray = [];
+    // calculate sum of numbers in array 
+    var aSumB = sum((sumArr[0]), (sumArr[1]));
+    aSumB = aSumB[0];
+    var finalSum = sum(aSumB, sumArr[2])[0];
+    // assign sum of numbers in array to the first index of the question4 return array
+    questionFourReturnArray[0] = finalSum;
+
+    // create string and assign to second index of returned array
+    questionFourReturnArray[1] = `${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${finalSum} is their sum.`
+    // return array
+    return questionFourReturnArray;
 }
+
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
